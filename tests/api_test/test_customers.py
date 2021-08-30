63d6a04ec93c03f12a0298eb7b1262b408df7483
seunkoko <seunowonikoko@gmail.com>
2021-08-30 16:13:34 +0300

    Create customer tests

--- tests/api_test/test_customers.py
+++ tests/api_test/test_customers.py
@@ -5,7 +5,7 @@ from tests.base import BaseTestCase
 
 
 class GetCustomersTestCase(BaseTestCase):
-    """ Test Data processing function """
+    """ Get Customers Route """
 
     def setUp(self):
         self.create_default_data() # process default data
@@ -51,3 +51,63 @@ class GetCustomersTestCase(BaseTestCase):
             2
         )
         self.assert200(response)
+
+
+class CreateCustomersTestCase(BaseTestCase):
+    """ Create Customer Route """
+
+    def setUp(self):
+        self.create_default_data() # process default data
+
+    def test_customer_already_exists(self):
+        """ Test create customer fail
+        POST /customers
+        """
+        customer = {
+            "customer": {
+                "id": 1,
+                "attributes": {
+                    "created_at": "1560964022",
+                    "email": "example@customer.io",
+                    "first_name": "example"
+                }
+            }
+        }
+
+        response = self.client.post('customers',
+            data=json.dumps(customer),
+            content_type='application/json'
+        )
+        response_data = json.loads(response.data)
+
+        self.assertEqual(response_data['message'], 'Customer ID already exists')
+        self.assert400(response)
+
+    def test_create_customer(self):
+        """ Test create customer success
+        POST /customers
+        """
+        customer = {
+            "customer": {
+                "id": 12345,
+                "attributes": {
+                    "created_at": "1560964022",
+                    "email": "example@customer.io",
+                    "first_name": "example"
+                }
+            }
+        }
+
+        response = self.client.post('customers',
+            data=json.dumps(customer),
+            content_type='application/json'
+        )
+        response_data = json.loads(response.data)
+
+        self.assertTrue(response_data['customer'])
+        self.assertEqual(response_data['customer']['id'], 12345)
+        self.assertEqual(response_data['customer']['attributes']['created_at'], '1560964022')
+        self.assertEqual(response_data['customer']['attributes']['email'], 'example@customer.io')
+        self.assertEqual(response_data['customer']['attributes']['first_name'], 'example')
+        self.assertTrue(response_data['customer']['last_updated'])
+        self.assertEqual(response.status_code, 201)
