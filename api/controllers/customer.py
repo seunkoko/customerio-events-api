045a643272c63dbf8571a07a47293923d5eb7a1f
seunkoko <seunowonikoko@gmail.com>
2021-08-30 16:30:52 +0300

    Delete customer data summary route

--- api/controllers/customer.py
+++ api/controllers/customer.py
@@ -11,7 +11,7 @@ from ..helper import (
 
 
 class UnitCustomerResource(Resource):
-    """Unit Customer Resource
+    """ Unit Customer Resource
     GET /customers/customer_id - Gets a customer's summary
     PATCH /customers/customer_id - Update customer's attribute summary
     DELETE /customers/customer_id - Delete customer's summary
@@ -32,3 +32,24 @@ class UnitCustomerResource(Resource):
         return {
             "customer": g.customers[customer_id]
         }, 200
+
+    @get_data_summary()
+    def delete(self, customer_id):
+        """Delete unit customer data"""
+
+        # check if customer exists
+        if customer_id not in list(g.customers.keys()):
+            return {
+                "message": "Customer ID does not exist"
+            }, 400
+
+        # delete customer
+        del g.customers[customer_id]
+
+        # update file
+        write_to_file(g.customers)
+
+        # return success message
+        return {
+            "message": "Customer info deleted successfully"
+        }, 201
