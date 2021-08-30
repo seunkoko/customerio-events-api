f82eabbf5198f014149bdc6e41de8cbaf036ffba
seunkoko <seunowonikoko@gmail.com>
2021-08-30 16:51:42 +0300

    Update customer attribute route

--- api/controllers/customer.py
+++ api/controllers/customer.py
@@ -33,6 +33,50 @@ class UnitCustomerResource(Resource):
             "customer": g.customers[customer_id]
         }, 200
 
+    @validate_request()
+    @get_data_summary()
+    def patch(self, customer_id):
+        """Update customer attribute"""
+
+        _data = request.get_json() # request body
+
+        # check if customer exists
+        if customer_id not in list(g.customers.keys()):
+            return {
+                "message": "Customer ID does not exist"
+            }, 400
+
+        # get customer summary
+        current_customer_details = g.customers[customer_id]
+
+        # check that immutable fields are not updated
+        immutable_fields = {'email', 'id', 'created_at'}
+        if set(_data['customer']['attributes'].keys()) & immutable_fields:
+            return {
+                "message": "Customer email, id or created_at fields cannot be updated."
+            }, 400
+
+        # prepare updated customer data
+        updated_customer = {
+            **current_customer_details,
+            'attributes': {
+                **current_customer_details['attributes'],
+                **_data['customer']['attributes']
+            },
+            'last_updated': round(time.time())
+        }
+
+        # update customer data
+        g.customers[customer_id] = updated_customer
+
+        # update file
+        write_to_file(g.customers)
+
+        # return success message
+        return {
+            "customer": updated_customer
+        }
+
     @get_data_summary()
     def delete(self, customer_id):
         """Delete unit customer data"""
